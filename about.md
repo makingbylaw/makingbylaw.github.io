585798208330245199ca6c162edcefb4aadb7059
Katy Law <klaw@cca.edu>
2014-09-28 12:10:12 -0700

    Intermediary changes

--- about.md
+++ about.md
@@ -4,7 +4,6 @@ title: THE DESIGNER
 permalink: /about/
 ---
 
-Born in Hong Kong, Katy Law is a visual communicator. Her interest in visual communication began in her childhood, before the days of Graphical User Interfaces. Katy's pastimes ranged from shape and symmetry studies in her colorings to storytelling through the composition of paper collages. Her quest to rethink solutions to exisiting problems has led her to complete her Bachelor's Degree in Interaction Design at CCA. Today, she defines and creates systems and interfaces to delight humans.
-
+B...
 
 
--- js/main.js
+++ js/main.js
@@ -11,9 +11,32 @@ $(function() {
         return false;
       }
     }
+
+    var sections = {},
+        _height  = $(window).height(),
+        i        = 0;
+    
+    // Grab positions of our sections 
+    $('.section').each(function(){
+        sections[this.name] = $(this).offset().top;
+    });
+
+    $(document).scroll(function(){
+        var $this = $(this),
+            pos   = $this.scrollTop();
+            
+        for(i in sections){
+            if(sections[i] > pos && sections[i] < pos + _height){
+                $('a').removeClass('active');
+                $('#nav_' + i).addClass('active');
+            }  
+        }
+    });
+});
+
   });
 
-  $(".page-content").onepage_scroll({
+  $(".content").onepage_scroll({
      sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
      easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
      animationTime: 1000, // AnimationTime let you define how long each section takes to animate
@@ -22,6 +45,7 @@ $(function() {
      beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
      afterMove: function(index) {}, // This option accepts a callback function. The function will be called after the page moves.
      loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-     responsiveFallback: 600 // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
+     responsiveFallback: 600, // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
+     direction: "vertical"
   });//@ sourceURL=pen.js
 });
