3161e7c313cac65e6195af025026bba4bc8f0ece
Katy Law <klaw@cca.edu>
2014-09-30 22:04:52 -0700

    Navigation Changes

--- js/main.js
+++ js/main.js
@@ -36,16 +36,52 @@ $(function() {
 
   });
 
-  $(".content").onepage_scroll({
-     sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
-     easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-     animationTime: 1000, // AnimationTime let you define how long each section takes to animate
-     pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-     updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-     beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
-     afterMove: function(index) {}, // This option accepts a callback function. The function will be called after the page moves.
-     loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-     responsiveFallback: 600, // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
-     direction: "vertical"
-  });//@ sourceURL=pen.js
+jQuery(document).ready(function($){
+  var contentSections = $('.cd-section'),
+    navigationItems = $('#cd-vertical-nav a');
+
+  updateNavigation();
+  $(window).on('scroll', function(){
+    updateNavigation();
+  });
+
+  //smooth scroll to the section
+  navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+      $('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+      $('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+  function updateNavigation() {
+    contentSections.each(function(){
+      $this = $(this);
+      var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+      if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+        navigationItems.eq(activeSection).addClass('is-selected');
+      }else {
+        navigationItems.eq(activeSection).removeClass('is-selected');
+      }
+    });
+  }
+
+  function smoothScroll(target) {
+        $('body,html').animate(
+          {'scrollTop':target.offset().top},
+          600
+        );
+  }
 });
